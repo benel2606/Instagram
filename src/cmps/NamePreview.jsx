bd8057c324ce04c65390816227f18104a235c900
benel2606 <benel2606@gmail.com>
2024-06-25 16:22:42 +0300

    fix image view in gh pages

--- src/cmps/NamePreview.jsx
+++ src/cmps/NamePreview.jsx
@@ -4,7 +4,7 @@ export function NamePreview({ by, time }) {
   return (
     <section className="name-preview">
       <div className="conatiner">
-        <img src={`/img${by.imgUrl}`} />
+        <img src={`img${by.imgUrl}`} />
         <a>{by.username}</a>
         <span>&bull;</span>
         <span>1h</span>
